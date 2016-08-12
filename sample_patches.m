4e5b6dd72a301e81dc341c13ad1477bea9220bd0
andfoy <andfoy@gmail.com>
2016-08-11 23:48:59 -0500

    Filter Training: Now extracting 100000 samples of size 16x16 from training set

--- sample_patches.m
+++ sample_patches.m
@@ -12,18 +12,32 @@ max_height = img_height - patch_size + 1;
 
 patches = zeros(patch_size*patch_size, num_patches);
 
-f_list = dir('train/*.tif');
+f_list = dir('data/train/*.tif');
 m = size(f_list, 1);
 
+reverseStr = '';
 for i = 1:num_patches
+    msg = sprintf('\bSampling patch %d from %d...\n', i, num_patches);
+    fprintf([reverseStr, msg]);
+    reverseStr = repmat(sprintf('\b'), 1, length(msg));
     img = f_list(randi(m)).name;
-    img = Tiff(['train/' img]);
+    img = Tiff(['data/train/' img]);
     img = img.read();
     w = randi(max_width);
     h = randi(max_height);
     patch = img(h:h+patch_size-1, w:w+patch_size-1);
     patches(:, i) = patch(:);
 end
+
+fprintf('Displaying sample patches...\n');
+
+num_samples = 100;
+
+s = randperm(num_patches);
+idx = s(1:num_samples);
+
+display_network(patches(:, idx));
+
 
 
 
